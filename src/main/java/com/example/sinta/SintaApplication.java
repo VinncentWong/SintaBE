a71f640d5a47e72d75c509d64142a4ee33634c67
VinncentWong <vinncentwong2@gmail.com>
2023-04-27 16:54:52 +0700

    Initial Commit

---
+++ src/main/java/com/example/sinta/SintaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sinta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SintaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sinta/SintaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sinta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SintaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
