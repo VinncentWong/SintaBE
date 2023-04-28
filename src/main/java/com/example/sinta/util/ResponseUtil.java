8ba4c46b286b224db98e6420cc763833c7fce0c3
VinncentWong <vinncentwong2@gmail.com>
2023-04-28 11:22:13 +0700

    [ADD] response util

---
+++ src/main/java/com/example/sinta/util/ResponseUtil.java
@@ -0,0 +1,27 @@
+package com.example.sinta.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public final class ResponseUtil {
+
+    public static final ResponseUtil INSTANCE = new ResponseUtil();
+    private ResponseUtil(){}
+
+    public ResponseEntity<Map<String, Object>> sendResponse(String message, HttpStatus status, Boolean success, Map<String, Object> data){
+        Map<String, Object> res = new LinkedHashMap<>();
+        res.put("message", message);
+        res.put("success", success);
+        if(data != null){
+            data.forEach((k, v) -> {
+                res.put(k, v);
+            });
+        } else {
+            res.put("data", null);
+        }
+        return ResponseEntity.status(status.value()).body(res);
+    }
+}
