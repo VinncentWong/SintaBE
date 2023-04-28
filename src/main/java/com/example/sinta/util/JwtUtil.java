8524dd2cc142ebdc4b5cab8305e20bed2f951685
VinncentWong <vinncentwong2@gmail.com>
2023-04-28 11:22:05 +0700

    [ADD] jwt util

---
+++ src/main/java/com/example/sinta/util/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.example.sinta.util;
+
+import com.example.sinta.domain.Extractable;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
+public final class JwtUtil {
+
+    /*
+    This is a random key that always generated every time start our application
+     */
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public static final JwtUtil INSTANCE = new JwtUtil();
+
+    private JwtUtil(){}
+
+    public String generateJwtToken(Extractable extractable){
+        return Jwts.builder()
+                .signWith(key)
+                .claim("id", extractable.getId())
+                .claim("nama", extractable.getNama())
+                .claim("createdAt", extractable.getCreatedAt())
+                .compact();
+    }
+}
