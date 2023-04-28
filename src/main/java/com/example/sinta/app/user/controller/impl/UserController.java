0e1ff77abc0c65f7b0e3edf9d00103ed44298d07
VinncentWong <vinncentwong2@gmail.com>
2023-04-28 11:25:18 +0700

    [ADD] user controller

---
+++ src/main/java/com/example/sinta/app/user/controller/impl/UserController.java
@@ -0,0 +1,54 @@
+package com.example.sinta.app.user.controller.impl;
+
+import com.example.sinta.app.user.controller.IUserController;
+import com.example.sinta.app.user.service.IUserService;
+import com.example.sinta.app.user.service.impl.UserService;
+import com.example.sinta.dto.UserDto;
+import com.example.sinta.exception.UserAlreadyExistException;
+import com.example.sinta.exception.WrongCredentialException;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController implements IUserController {
+
+    private final IUserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping(
+            value = "/create",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Override
+    public ResponseEntity<Map<String, Object>> createUser(@RequestBody @Valid UserDto.Create dto) throws UserAlreadyExistException {
+        return this.service.createUser(dto);
+    }
+
+    @PostMapping(
+            value = "/login",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Override
+    public ResponseEntity<Map<String, Object>> loginUser(@RequestBody @Valid UserDto.Login dto) throws WrongCredentialException {
+        return this.service.loginUser(dto);
+    }
+
+    @GetMapping(
+            value = "/update/verifikasi/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Override
+    public ResponseEntity<Map<String, Object>> updateVerifikasiUser(@PathVariable("id") Long id) {
+        return this.service.updateVerifikasiUser(id);
+    }
+}
