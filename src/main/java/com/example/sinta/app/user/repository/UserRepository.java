336156123e40ac48c36c57ffa0055bd2200d3a0a
VinncentWong <vinncentwong2@gmail.com>
2023-04-28 11:25:02 +0700

    [ADD] user repository

---
+++ src/main/java/com/example/sinta/app/user/repository/UserRepository.java
@@ -0,0 +1,42 @@
+package com.example.sinta.app.user.repository;
+
+import com.example.sinta.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    @Query(
+            nativeQuery = true,
+            value = """
+                    UPDATE users
+                    SET verified=:verifikasi
+                    WHERE id=:id
+                    """
+    )
+    @Modifying
+    void updateVerifikasiUser(@Param("id") Long id, @Param("verifikasi") int verifikasi);
+
+    @Query(
+            nativeQuery = true,
+            value = """
+                    SELECT * FROM users WHERE email=:email LIMIT 1
+                    """
+    )
+    Optional<User> findUserByEmail(@Param("email") String email);
+
+    @Query(
+            nativeQuery = true,
+            value = """
+                    SELECT * FROM users WHERE no_telp=:noTelp LIMIT 1
+                    """
+    )
+    Optional<User> findUserByNoTelp(@Param("noTelp") String noTelp);
+}
