baaa9dd7f9ef7dccf6bdc45a35a2482076d5c954
VinncentWong <vinncentwong2@gmail.com>
2023-05-05 13:39:08 +0700

    [ADD] new service

--- src/main/java/com/example/sinta/app/user/service/IUserService.java
+++ src/main/java/com/example/sinta/app/user/service/IUserService.java
@@ -2,15 +2,16 @@ package com.example.sinta.app.user.service;
 
 import com.example.sinta.dto.UserDto;
 import com.example.sinta.exception.UserAlreadyExistException;
+import com.example.sinta.exception.UserNotFoundException;
 import com.example.sinta.exception.WrongCredentialException;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
 
 public interface IUserService {
-
     ResponseEntity<Map<String, Object>> createUser(UserDto.Create dto) throws UserAlreadyExistException;
     ResponseEntity<Map<String, Object>> loginUser(UserDto.Login dto) throws WrongCredentialException;
     ResponseEntity<Map<String, Object>> updateVerifikasiUser(Long id);
-
+    ResponseEntity<Map<String, Object>> getUser(Long id) throws UserNotFoundException;
+    ResponseEntity<Map<String, Object>> updateUser(Long id, UserDto.Update dto) throws UserNotFoundException, UserAlreadyExistException; 
 }
