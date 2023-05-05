9a979c57f2cd4b868f8d2c677947a3fc84f0c332
VinncentWong <vinncentwong2@gmail.com>
2023-05-05 13:39:45 +0700

    [ADD] new controller

--- src/main/java/com/example/sinta/app/user/controller/IUserController.java
+++ src/main/java/com/example/sinta/app/user/controller/IUserController.java
@@ -2,6 +2,7 @@ package com.example.sinta.app.user.controller;
 
 import com.example.sinta.dto.UserDto;
 import com.example.sinta.exception.UserAlreadyExistException;
+import com.example.sinta.exception.UserNotFoundException;
 import com.example.sinta.exception.WrongCredentialException;
 import org.springframework.http.ResponseEntity;
 
@@ -10,4 +11,6 @@ public interface IUserController {
     ResponseEntity<Map<String, Object>> createUser(UserDto.Create dto) throws UserAlreadyExistException;
     ResponseEntity<Map<String, Object>> loginUser(UserDto.Login dto) throws WrongCredentialException;
     ResponseEntity<Map<String, Object>> updateVerifikasiUser(Long id);
+    ResponseEntity<Map<String, Object>> getUser(Long id) throws UserNotFoundException;
+    ResponseEntity<Map<String, Object>> updateUser(Long id, UserDto.Update dto) throws UserNotFoundException, UserAlreadyExistException; 
 }
