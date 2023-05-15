bbec0670d246c252574e9fda633558763108ad18
VinncentWong <vinncentwong2@gmail.com>
2023-05-15 10:34:49 +0700

    [ADD] several new exception

---
+++ src/main/java/com/example/sinta/exception/BankNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.sinta.exception;
+
+public class BankNotFoundException extends Exception{
+    
+    public BankNotFoundException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/sinta/exception/HistoryPembelianPremiumNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.sinta.exception;
+
+public class HistoryPembelianPremiumNotFoundException extends Exception{
+    
+    public HistoryPembelianPremiumNotFoundException(String message){
+        super(message);
+    }
+}
